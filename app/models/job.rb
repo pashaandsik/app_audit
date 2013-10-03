a6cddfd12a4e561f8e82860638b5449a61d91295
pasha <pashaandsik@mail.ru>
2013-10-03 18:48:36 +0400

    бррр

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,10 @@ class Job < ActiveRecord::Base
   belongs_to :car
   has_many :details, through: :car
   attr_accessible :km_n, :m_ch, :repair, :to , :car_id
+  after_save :update_details_id
+  def update_details_id
+    @inire=car_id
+    p "-------------------------"
+    p Detail.joins(:car, :jobs)
+  end
 end
