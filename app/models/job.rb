01eabdc857a709e5f699e08b5a77d3286ae845e2
pasha <pashaandsik@mail.ru>
2013-10-04 19:00:30 +0400

    перебор

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,7 +4,11 @@ class Job < ActiveRecord::Base
   attr_accessible :km_n, :m_ch, :repair, :to , :car_id
   after_save :update_details_id
   def update_details_id
-    inire=car_id
-    Car.joins("LEFT JOIN jobs ON cars.id = jobs.car_id").joins("LEFT JOIN details ON cars.id = details.car_id").where('details.car_id' =>inire)
+    Detail.where('details.car_id' => car_id).each do |detail|
+      detail.job_id = id
+
+      detail.save
+    end
+    #Car.joins("LEFT JOIN jobs ON cars.id = jobs.car_id").joins("LEFT JOIN details ON cars.id = details.car_id").where('details.car_id' =>inire)
   end
 end
